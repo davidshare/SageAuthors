b3f6ef55cf42d63cb6c817f2f17a5d3944db0436
davidshare <davidessienshare@gmail.com>
2020-02-01 09:40:54 +0100

    ch(testing): implement testing with jest
    - add the index.js file and write tests for it
    - create the api directory and add the home route

---
+++ server/index.js
@@ -0,0 +1,46 @@
+// server.js
+import express from 'express';
+import cors from 'cors';
+// import connection from './helpers/db_conn';
+import routes from './api/v1';
+
+// const client = connection();
+// client.connect();
+
+const port = process.env.PORT || 5001;
+
+// client.connect();
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+routes(app);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+/* eslint-disable-next-line */
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    errors: {
+      message: err.message
+    }
+  });
+});
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port);
+}
+
+/* eslint-disable-next-line */
+console.log('app running on port ', port);
+
+export default app;
